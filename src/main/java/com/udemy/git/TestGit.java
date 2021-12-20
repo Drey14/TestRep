6886514fa63df426a3e101800aa0d8fbc58ceb16
”Andrey <Soda14@mail.ru>
2021-12-21 00:47:03 +0300

    calc ver 4.0 VALUE A=100 method del

--- src/main/java/com/udemy/git/TestGit.java
+++ src/main/java/com/udemy/git/TestGit.java
@@ -3,23 +3,25 @@ package com.udemy.git;
 public class TestGit {
 
     public static void main(String[] args) {
-        int a = 10;
+        int a = 100;
         int b = 2;
 
         new TestGit().add(a,b);
         new TestGit().sub(a,b);
         new TestGit().mod(a,b);
+        new TestGit().del(a,b);
     }
 
     void add(int a, int b) {
         System.out.println(a+b);
     }
-
     void sub(int a, int b) {
         System.out.println(a-b);
     }
     void mod(int a, int b) {
         System.out.println(a*b);
     }
-
+    void del(int a, int b) {
+        System.out.println(a/b);
+    }
 }
