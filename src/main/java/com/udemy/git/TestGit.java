d97955bc736dfb677f9c02ecdb0696b5654eadc5
”Andrey <Soda14@mail.ru>
2021-12-21 00:32:37 +0300

    calc ver 1.0 method add

---
+++ src/main/java/com/udemy/git/TestGit.java
@@ -0,0 +1,18 @@
+package com.udemy.git;
+
+public class TestGit {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+
+        new TestGit().add(a,b);
+
+
+    }
+
+    void add(int a, int b) {
+        System.out.println(a+b);
+    }
+
+}
